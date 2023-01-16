47e3dd497b2d8c6953ddae7fb57f3308a2bf0926
Nicholas Zhuk <nicholaszhuk@gmail.com>
2023-01-15 23:48:24 -0800

    Added circlechecker

--- main.js
+++ main.js
@@ -6,17 +6,18 @@ ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-
+	var idMaker = 0;
 	for( i = 0; i < 35; i++){
 		for(j = 0; j < 35; j++){
-			var enemy = new Enemy(gameEngine)
+			var enemy = new Enemy(gameEngine,idMaker)
 			enemy.x = 400 + (i*6)
 			enemy.y = 400 + (j*6)
 			gameEngine.addEntity(enemy)
+			idMaker++
 		}
 
 	}
-	gameEngine.entities[0].size = 30
+	gameEngine.entities[0].radius = 15
 
 
 
