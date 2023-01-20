8ca2435e17437f6eccaca9e7369288077968b13c
Kevin Nguyen <nguyekev@uw.edu>
2023-01-19 18:29:05 -0800

    img

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./imag/Luffy.png")
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
