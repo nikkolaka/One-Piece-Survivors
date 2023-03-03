5d6cc52524bc72fdadb695dba759821e972e56e8
Jashanpreet Jandu <jashanjandu23@gmail.com>
2023-03-03 10:37:35 -0800

    New map and collisions fixed

--- background.js
+++ background.js
@@ -10,24 +10,21 @@ class Background{
         this.width = 5000;
         this.height = 5000;
 
-        this.leftEdge = 512;
-        this.rightEdge = 512;
+        this.leftEdge = 5000;
+        this.rightEdge = 5000;
      
         this.spritesheet = ASSET_MANAGER.getAsset("./img/OnePieceMap.png"); 
     };
 
     update() {
+        // if (this.x > 450) this.x = 450;
+        // else if (this.x < -4600) this.x = -4600;
 
-
-        if (this.x > 450) this.x = 450;
-        else if (this.x < -4600) this.x = -4600;
-
-        if (this.y > 326) this.y = 326;
-        else if (this.y < -4706) this.y = -4706;
+        // if (this.y > 326) this.y = 326;
+        // else if (this.y < -4706) this.y = -4706;
     };
 
     draw(ctx) {
-        ctx.drawImage(this.spritesheet, 0, 0, 5000, 5000, this.x - this.game.camera.x, this.y - this.game.camera.y, 5120, 5120);
+        ctx.drawImage(this.spritesheet, 0, 0, 8000, 8000, this.x - this.game.camera.x, this.y - this.game.camera.y, 10000, 10000);
     };
-
 }
