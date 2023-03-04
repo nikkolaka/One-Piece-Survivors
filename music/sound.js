860e7843243b2d8a26518808247492164c4ecc90
Jashanpreet Jandu <jashanjandu23@gmail.com>
2023-03-04 11:00:29 -0800

    sounds

--- music/sound.js
+++ music/sound.js
@@ -1,25 +1,31 @@
 class Sound {
-}    constructor(src) {
+    constructor(src) {
         this.sound = document.createElement("audio");
         this.sound.src = src;
         this.sound.setAttribute("preload", "auto");
         this.sound.setAttribute("controls", "none");
-
         this.sound.style.display = "none";
         document.body.appendChild(this.sound);
     }
-
     play() {
         this.sound.play();
     }
-
     stop() {
         this.sound.pause();
     }
 
     loop() {
         this.sound.loop = true;
+        this.sound.play();
+    }
+
+    stopLoop() {
+
+        this.sound.loop = false;
+        this.sound.pause();
+    }
+
+    setVolume(volume) {
+        this.sound.volume = volume;
     }
 }
-
-// Path: music\music.js
